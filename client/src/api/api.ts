878758f147a7c550f3b0b6effd2e131d4801b05d
MarEeee <pro100efim@gmail.com>
2022-05-28 12:31:47 +0100

    feat: add basic api structure

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,8 +1,41 @@
 import instance from "./instance"
 
 
+interface IAuthRequest {
+    email: string,
+    password: string
+}
+
+interface IRegRequest extends IAuthRequest{
+    username: string
+}
+
 export const API = {
-    getUsers(){
-        return instance.get('users')
+    fetchAuth (request: IAuthRequest){
+        return instance.post('/login', request)
+    },
+    fetchReg (request: IRegRequest) {
+        return instance.post('/registration', request)  
+    },
+    sighOut() {
+        return instance.get('/logout')
+    },
+    fetchCreateNewTask(newTask: any) {
+        return instance.post('/tasks', newTask)
+    },
+    fetchTasks() {
+        return instance.get('/tasks')
+    },
+    fetchTaskStatuses () {
+        return instance.get('/statuses')
+    },
+    fetchTaskPriority() {
+        return instance.get('/priority')
+    },
+    fetchUsers() {
+        return instance.get('/users')
+    },
+    fetchBoards() {
+        return instance.get('/board')
     }
 }
