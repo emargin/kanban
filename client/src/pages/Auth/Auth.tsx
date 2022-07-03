871cb54de759336aa277f972451e100c3fca9c53
Efim <pro100efim@gmail.com>
2022-07-03 17:22:14 +0500

    feat: 🌳 add basic login and reg pages

--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -3,76 +3,46 @@ import React, { FC } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Auth.module.scss'
 
-const Auth = ():JSX.Element => {
+const Auth = (): JSX.Element => {
     return (
         <section className={styles.loginPage}>
             <div className={styles.loginWindow}>
                 <form className={styles.form}>
-                    <h2 className={styles.loginTitle}>Авторизация</h2>
-                    {/* <Controller
-                        name="email"
-                        control={control}
-                        render={({ field }) => (
-                            <TextField
-                                {...field}
-                                id="outlined-basic"
-                                variant="outlined"
-                                className={styles.input}
-                                InputLabelProps={{
-                                    classes: {
-                                        root: styles.cssLabel,
-                                        focused: styles.cssFocused
-                                    },
-                                    className: styles.resize
-                                }}
-                                InputProps={{
-                                    classes: {
-                                        root: styles.cssOutlinedInput,
-                                        focused: styles.cssFocused,
-                                        notchedOutline: styles.notchedOutline
-                                    }
-                                }}
-                                helperText={errors.email?.message || ''}
-                                error={!!errors.email?.message}
-                                autoComplete="off"
-                                label="E-mail"
-                                type="text"
-                            />
-                        )}
+                    <h2 className={styles.title}>Авторизация</h2>
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        className={styles.input}
+                        autoComplete="off"
+                        label="E-mail"
+                        type="text"
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        className={styles.input}
+                        InputLabelProps={{
+                            classes: {
+                                root: styles.cssLabel,
+                                focused: styles.cssFocused
+                            },
+                            className: styles.resize
+                        }}
+                        InputProps={{
+                            classes: {
+                                root: styles.cssOutlinedInput,
+                                focused: styles.cssFocused,
+                                notchedOutline: styles.notchedOutline
+                            },
+                            inputMode: 'numeric'
+                        }}
+                        // helperText={errors.password?.message || ''}
+                        // error={!!errors.password?.message}
+                        autoComplete="off"
+                        label="Пароль"
+                        type="password"
                     />
 
-                    <Controller
-                        name="password"
-                        control={control}
-                        render={({ field }) => (
-                            <TextField
-                                {...field}
-                                id="outlined-basic"
-                                variant="outlined"
-                                className={styles.input}
-                                InputLabelProps={{
-                                    classes: {
-                                        root: styles.cssLabel,
-                                        focused: styles.cssFocused
-                                    },
-                                    className: styles.resize
-                                }}
-                                InputProps={{
-                                    classes: {
-                                        root: styles.cssOutlinedInput,
-                                        focused: styles.cssFocused,
-                                        notchedOutline: styles.notchedOutline
-                                    },
-                                    inputMode: 'numeric'
-                                }}
-                                helperText={errors.password?.message || ''}
-                                error={!!errors.password?.message}
-                                autoComplete="off"
-                                label="Пароль"
-                                type="password"
-                            />
-                        )}
-                    /> */}
                     <Button className={styles.authButton} variant="contained" type="submit">
                         Войти
                     </Button>
--- client/src/pages/Registration/Registration.tsx
+++ client/src/pages/Registration/Registration.tsx
@@ -1,8 +1,138 @@
+import { Button, TextField } from '@material-ui/core'
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
 import styles from './Registration.module.scss'
 
-const Registration = ():JSX.Element => {
-    return <></>
+const Registration = (): JSX.Element => {
+    return (
+        <div className={styles.root}>
+            <div className={styles.regWindow}>
+                <form
+                    className={styles.form}
+                // onSubmit={handleSubmit(handlerForm)}
+                >
+                    <h2 className={styles.loginTitle}>Регистрация</h2>
+
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        className={styles.input}
+                        InputLabelProps={{
+                            classes: {
+                                root: styles.cssLabel,
+                                focused: styles.cssFocused,
+                            },
+                            className: styles.resize
+                        }}
+                        InputProps={{
+                            classes: {
+                                root: styles.cssOutlinedInput,
+                                focused: styles.cssFocused,
+                                notchedOutline: styles.notchedOutline,
+                            }
+                        }}
+
+                        // helperText={(errors.name?.message) || ''}
+                        // error={!!errors.name?.message}
+                        autoComplete="off"
+                        label="Имя пользователя"
+                        type="text"
+                    />
+
+
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        className={styles.input}
+                        InputLabelProps={{
+                            classes: {
+                                root: styles.cssLabel,
+                                focused: styles.cssFocused,
+                            },
+                            className: styles.resize
+                        }}
+                        InputProps={{
+                            classes: {
+                                root: styles.cssOutlinedInput,
+                                focused: styles.cssFocused,
+                                notchedOutline: styles.notchedOutline,
+                            }
+                        }}
+
+                        // helperText={(errors.email?.message) || ''}
+                        // error={!!errors.email?.message}
+                        autoComplete="off"
+                        label="E-mail"
+                        type="text"
+                    />
+
+
+
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        className={styles.input}
+
+                        InputLabelProps={{
+                            classes: {
+                                root: styles.cssLabel,
+                                focused: styles.cssFocused,
+                            },
+                            className: styles.resize
+                        }}
+                        InputProps={{
+                            classes: {
+                                root: styles.cssOutlinedInput,
+                                focused: styles.cssFocused,
+                                notchedOutline: styles.notchedOutline,
+                            },
+                            inputMode: "numeric"
+                        }}
+
+                        // helperText={(errors.password?.message) || ''}
+                        // error={!!errors.password?.message}
+                        autoComplete="off"
+                        label="Пароль"
+                        type="password"
+                    />
+
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        className={styles.input}
+
+                        InputLabelProps={{
+                            classes: {
+                                root: styles.cssLabel,
+                                focused: styles.cssFocused,
+                            },
+                            className: styles.resize
+                        }}
+                        InputProps={{
+                            classes: {
+                                root: styles.cssOutlinedInput,
+                                focused: styles.cssFocused,
+                                notchedOutline: styles.notchedOutline,
+                            },
+                            inputMode: "numeric"
+                        }}
+
+                        // helperText={(errors.password?.message) || ''}
+                        // error={!!errors.password?.message}
+                        autoComplete="off"
+                        label="Подтверждение пароля"
+                        type="password"
+                    />
+
+
+                    <Button className={styles.regButton} variant="contained" type="submit" >
+                        Регистрация
+                    </Button>
+                    <Link to='/auth' className={styles.helperLink}>Вход</Link>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Registration
